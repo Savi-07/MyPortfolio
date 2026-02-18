64c900d80eeea9791c14fc8482484d7c5a39bd46
Sahil Kumar Singh <131599038+Savi-07@users.noreply.github.com>
2026-02-18 17:39:58 +0530

    Update hero section description text

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,7 +6,7 @@ const HeroSection = () => (
       <h1 className="hero-title-stroke">SAHIL</h1>
       <h1 className="hero-title-stroke">KUMAR</h1>
       <h1 className="hero-title-stroke">SINGH</h1>
-      <p>DEVELOPER | BUILDER | PROBLEM SOLVER</p>
+      <p>DEVELOPER X PROBLEM SOLVER</p>
     </div>
     {/* <div className="scroll-indicator">🢃</div> */}
   </section>
